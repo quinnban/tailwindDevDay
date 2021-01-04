713910dd1a6d0bcedc1ee48aaf29d72cc9cb8f18
Quinn <quinn@dev-pro.ca>
2021-01-04 12:28:58 -0600

    chip component

--- angular-tailwind/src/app/app.module.ts
+++ angular-tailwind/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChipComponent } from './chip/chip.component';
+import { AvatarComponent } from './chip/componets/avatar/avatar.component';
+import { SvgButtonComponent } from './chip/componets/svg-button/svg-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChipComponent,
+    AvatarComponent,
+    SvgButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ angular-tailwind/src/app/chip/componets/svg-button/svg-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-button',
+  templateUrl: './svg-button.component.html',
+  styleUrls: ['./svg-button.component.scss']
+})
+export class SvgButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
